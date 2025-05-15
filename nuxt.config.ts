245130e1a5ca28f702c409914d21021fbc5f0948
Jarek Ostrowski <jarek@distromfg.com>
2025-05-15 11:18:46 -0400

    Fix capitalized apps in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "We build AI Apps",
+      title: "We build AI apps",
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: 'Software development agency who builds AI apps for startups and businesses.' },
 
         // Open Graph (OG) tags
-        { property: 'og:title', content: 'We build AI Apps' },
+        { property: 'og:title', content: 'We build AI apps' },
         { property: 'og:description', content: 'Software development agency who builds AI apps for startups and businesses.' },
         { property: 'og:image', content: 'https://webuildaiapps.com/images/twitter-card.png' },
         { property: 'og:url', content: 'https://webuildaiapps.com' },
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
 
         // Twitter Card tags
         { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:title', content: 'We build AI Apps' },
+        { name: 'twitter:title', content: 'We build AI apps' },
         { name: 'twitter:description', content: 'Software development agency who builds AI apps for startups and businesses.' },
         { name: 'twitter:image', content: 'https://webuildaiapps.com/images/twitter-card.png' },
       ],
